ec7047fa78f1b8d5a48a47624400deb3b38dc80b
Jiwon Park <minerva1993@gmail.com>
2020-02-03 21:57:57 +0900

    upto jerc

--- macro/cuts.py
+++ macro/cuts.py
@@ -1,20 +1,38 @@
 import collections
 
-def cut_flow():
+def cut_flow(bdiscr):
 
+  b_discr = str(bdiscr)
   d = collections.OrderedDict()
-  d['0'] = 'channel >= 0'
-  d['1'] = 'njets == 3'
-  d['2'] = 'njets == 3 && nbjets_m == 2'
-  d['3'] = 'njets == 3 && nbjets_m == 3'
-  d['4'] = 'njets >= 3 && nbjets_m >= 2'
-  d['5'] = 'njets >= 4'
-  d['6'] = 'njets >= 4 && nbjets_m == 2'
-  d['7'] = 'njets >= 4 && nbjets_m == 3'
-  d['8'] = 'njets >= 4 && nbjets_m == 4'
+  d['0'] = ''
+  d['1'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) == 3'
+  d['2'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) == 3\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') == 2'
+  d['3'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) == 3\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') == 3'
+  d['4'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) >= 3\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') >= 2'
+  d['5'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) >= 4'
+  d['6'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) >= 4\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') == 2'
+  d['7'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) >= 4\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') == 3'
+  d['8'] = ' && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4) >= 4\
+             && Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_discr + ') == 4'
 
   return d
 
+
+def lepton_sel(year):
+
+  d = collections.OrderedDict()
+  if year >= 2017:
+    d['0'] = '(channel == 0 && lepton_pt > 30 && abs(lepton_eta) <= 2.4)'
+    d['1'] = '(channel == 1 && lepton_pt > 30 && abs(lepton_eta) <= 2.4)'
+    d['2'] = '((channel == 0 && lepton_pt > 30 && abs(lepton_eta) <= 2.4)\
+             || (channel == 1 && lepton_pt > 30 && abs(lepton_eta) <= 2.4))'
+
+    return d
 
 
 def b_tagging(year):
--- macro/run_ana.py
+++ macro/run_ana.py
@@ -24,24 +24,53 @@ outDir = '../output/' + str(options.year)
 default_branch = ['channel','lepton_pt']
 list_to_process = dataset(options.year)
 
+syst1 = ['','jecup','jecdown','jerup','jerdown']
+syst2 = ['TuneCP5up','TuneCP5down','hdampup','hdampdown'] #external sample
+
 for folder_to_process in list_to_process:
   if folder_to_process != "TT_powheg_ttbb": continue
   d = load_file( get_file_list(rootDir[options.year], folder_to_process), default_branch )
-  out = TFile.Open( os.path.join(outDir, folder_to_process)+'.root', 'RECREATE' )
 
-  cut_dict = cut_flow()
-  #print(d)
-  for c_key, c_value in cut_dict.items():
-    if c_key != '0': continue
-    d.Filter(c_value, c_key)
+  #Deal with JER/C, Hdamp, Tune systematics
+  loc_str = os.path.join(rootDir[options.year], folder_to_process)
+  for syst_ext in syst1 + syst2:
+    if ('Run201' in loc_str) and syst_ext != '': continue
+    elif (syst_ext in syst2) and not (syst_ext in loc_str): continue
+    elif (syst_ext in syst1) and any(tmp in loc_str for tmp in syst2): continue
+    else:
+      #plotIt convention
+      if (syst_ext in syst2): name = folder_to_process.replace('_' + syst_ext,'')
 
-    hist_dict = histos(0, c_key, '')
+    if syst_ext == "": postfix = ""
+    else:              postfix = "__"
 
-    for h_key, h_value in hist_dict.items():
-      h = d.Histo1D(h_value, h_key)
-      h.Write()
+    #JER/C treatment
+    jec_str = ''
+    if   syst_ext == 'jecup'  : jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Up'
+    elif syst_ext == 'jecdown': jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Down'
+    elif syst_ext == 'jerup'  : jec_str = 'jet_pt * jet_JER_Up'
+    elif syst_ext == 'jerdown': jec_str = 'jet_pt * jet_JER_Down'
+    else:                       jec_str = 'jet_pt * jet_JER_Nom'
 
-  out.Close()
+    #Create output root file
+    out = TFile.Open( os.path.join(outDir, folder_to_process) + postfix + syst_ext + '.root', 'RECREATE' )
+
+    #Cut flow, lepton first
+    lepton_dict = lepton_sel(options.year)
+    for l_key, l_value in lepton_dict.items():
+
+      #Be careful of the working point!
+      cut_dict = cut_flow(b_tagging(options.year))
+      #print(d)
+
+      for c_key, c_value in cut_dict.items():
+        hist_dict = histos(l_key, c_key, '')
+
+        for h_key, h_value in hist_dict.items():
+          h = d.Define('jet_pt_jerc', jec_str).Filter(l_value + c_value, c_key).Histo1D(h_value, h_key)
+          h.Write()
+
+    out.Close()
 
 print(process.memory_info()[0]/(1024.0*1024)) #rss only
 print(process.cpu_times())
