ba7880c9219fcee087531b7914cd74268ff3c5a4
Jiwon Park <minerva1993@gmail.com>
2020-02-21 14:32:32 +0900

    more histos for test

--- macro/histos.py
+++ macro/histos.py
@@ -4,6 +4,19 @@ import collections
 def histos(ch, step, syst_name):
 
   d = collections.OrderedDict()
-  d['lepton_eta'] = ROOT.RDF.TH1DModel('h_LepEta_Ch{}_S{}{}'.format(ch, step, syst_name),';Lepton |#eta|;Entries',30,-3,3)
+  d['GoodPV'] = ROOT.RDF.TH1DModel('h_PV_Ch{}_S{}{}'.format(ch, step, syst_name), ';Good PV;Entries', 60, 0, 60)
+  d['lepton_eta'] = ROOT.RDF.TH1DModel('h_LepEta_Ch{}_S{}{}'.format(ch, step, syst_name), ';Lepton |#eta|;Entries', 30, -3, 3)
+  d['njet_jerc'] = ROOT.RDF.TH1DModel('h_NJet_Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', 12, 0, 12)
+  d['nbjet_jerc'] = ROOT.RDF.TH1DModel('h_NBJetCSVv2M_Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', 6, 0, 6)
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
+  #d[''] = ROOT.RDF.TH1DModel('h__Ch{}_S{}{}'.format(ch, step, syst_name), ';;Entries', , , )
 
   return d
+
