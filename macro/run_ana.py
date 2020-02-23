cd670ff4dcd6e9f6d458c3b3a4a330a3ad9676a6
Jiwon Park <minerva1993@gmail.com>
2020-02-24 01:27:09 +0900

    multiprocessing with python

--- macro/run_ana.py
+++ macro/run_ana.py
@@ -1,6 +1,6 @@
 from ROOT import *
 import ROOT
-import os
+import os, sys, multiprocessing
 import argparse, psutil
 
 from analysis_helper import *
@@ -17,87 +17,112 @@ process = psutil.Process(os.getpid())
 #ROOT.ROOT.EnableImplicitMT(4)
 gROOT.SetBatch()
 
+year = options.year
 rootDir = {2017:'/data/users/minerva1993/ntuple/V9_6/200101/production',
            2018:'/data/users/minerva1993/ntuple/V10_3/200101/production'}
-outDir = '../output/' + str(options.year)
+outDir = '../output/' + str(year)
 
 default_branch = ['channel','lepton_pt']
-list_to_process = dataset(options.year)
+list_to_process = dataset(year)
 
 syst1 = ['','jecup','jecdown','jerup','jerdown']
 syst2 = ['TuneCP5up','TuneCP5down','hdampup','hdampdown'] #external sample
 
-for folder_to_process in list_to_process:
-  if folder_to_process != "TT_powheg_ttbb": continue
+#Create list of tuple (folder_to_process, syst_ext)
+list_args = []
+for f_tmp in list_to_process:
+  for s_tmp in syst1 + syst2:
+    list_args.append((f_tmp, s_tmp))
+
+def run_ana(tuple_args):
+  #if folder_to_process != "TT_powheg_ttbb": continue
+
+  folder_to_process = tuple_args[0]
+  folder_to_process = folder_to_process.replace('_','')
+  syst_ext = tuple_args[1]
+
+  isData = False
+  if 'Run201' in folder_to_process: isData = True
 
   #Deal with JER/C, Hdamp, Tune systematics
-  loc_str = os.path.join(rootDir[options.year], folder_to_process)
-  for syst_ext in syst1 + syst2:
-    if ('Run201' in loc_str) and syst_ext != '': continue
-    elif (syst_ext in syst2) and not (syst_ext in loc_str): continue
-    elif (syst_ext in syst1) and any(tmp in loc_str for tmp in syst2): continue
-    else:
-      #plotIt convention
-      if (syst_ext in syst2): name = folder_to_process.replace('_' + syst_ext,'')
+  loc_str = os.path.join(rootDir[year], folder_to_process)
+  if ('Run201' in loc_str) and syst_ext != '': return
+  elif (syst_ext in syst2) and not (syst_ext in loc_str): return
+  elif (syst_ext in syst1) and any(tmp in loc_str for tmp in syst2): return
 
-    if syst_ext == "": postfix = ""
-    else:              postfix = "__"
+  if syst_ext == "": postfix = ""
+  else:              postfix = "__"
 
-    #JER/C treatment
-    jec_str = ''
-    if   syst_ext == 'jecup'  : jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Up'
-    elif syst_ext == 'jecdown': jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Down'
-    elif syst_ext == 'jerup'  : jec_str = 'jet_pt * jet_JER_Up'
-    elif syst_ext == 'jerdown': jec_str = 'jet_pt * jet_JER_Down'
-    else:                       jec_str = 'jet_pt * jet_JER_Nom'
+  #JER/C treatment
+  jec_str = ''
+  if   syst_ext == 'jecup'  : jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Up'
+  elif syst_ext == 'jecdown': jec_str = 'jet_pt * jet_JER_Nom * jet_JES_Down'
+  elif syst_ext == 'jerup'  : jec_str = 'jet_pt * jet_JER_Up'
+  elif syst_ext == 'jerdown': jec_str = 'jet_pt * jet_JER_Down'
+  else:                       jec_str = 'jet_pt * jet_JER_Nom'
 
-    hists = []
-    #Open dataframe and create output root file
-    d = load_file( get_file_list(rootDir[options.year], folder_to_process), default_branch )
-    out = TFile.Open( os.path.join(outDir, 'hist_' + folder_to_process.replace('_','')) + postfix + syst_ext + '.root', 'RECREATE' )
-    print "Creating " + os.path.join(outDir, 'hist_' + folder_to_process.replace('_','')) + postfix + syst_ext + '.root'
+  hists = []
+  #Open dataframe and create output root file
+  d = load_file( get_file_list(rootDir[year], tuple_args[0]), default_branch )
+  out = TFile.Open( os.path.join(outDir, 'hist_' + folder_to_process.replace(syst_ext,'')) + postfix + syst_ext + '.root', 'RECREATE' )
 
-    #Cut flow, lepton first
-    d = d.Define('lep_sel', '(channel == 0 && lepton_pt > 30 && abs(lepton_eta) <= 2.4) ||\
-                             (channel == 1 && lepton_pt > 30 && abs(lepton_eta) <= 2.4)').Filter('lep_sel > 0')
+  #Cut flow, lepton first
+  d = d.Define('lep_sel', '(channel == 0 && lepton_pt > 30 && abs(lepton_eta) <= 2.4) ||\
+                           (channel == 1 && lepton_pt > 30 && abs(lepton_eta) <= 2.4)').Filter('lep_sel > 0')
 
-    #Jec/jer
-    d = d.Define('jet_pt_jerc', jec_str)
-    #tmp = ROOT.vector("string")()
-    #tmp.push_back('jet_pt_jerc')
-    #pp = d.Display(tmp)
-    #pp.Print()
+  #Define EventWeight
+  if isData: d = d.Define('EventWeight', '1')
+  #else: d = d.Define('EventWeight', wrongPVrate(folder_to_process, year))
+  else: d = d.Define('EventWeight', 'genweight')
+    
 
-    d = d.Define('njet_jerc', 'Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4)')\
-         .Define('nbjet_jerc', 'Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_tagging(options.year) + ')')
+  #Jec/jer
+  if isData: d = d.Define('jet_pt_jerc', 'jet_pt')
+  else: d = d.Define('jet_pt_jerc', jec_str)
+  #tmp = ROOT.vector("string")()
+  #tmp.push_back('jet_pt_jerc')
+  #pp = d.Display(tmp)
+  #pp.Print()
 
-    #Run on step0 first
-    lepton_dict = lepton_sel(options.year)
+  d = d.Define('njet_jerc', 'Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4)')\
+       .Define('nbjet_jerc', 'Sum(jet_pt_jerc > 30 && abs(jet_eta) < 2.4 && jet_deepCSV > ' + b_tagging(year) + ')')
+
+  #Run on step0 first
+  syst_int = ''
+  lepton_dict = lepton_sel(year)
+  for l_key, l_value in lepton_dict.items():
+    hist_dict = histos(l_key, 0, postfix + syst_int)
+
+    for h_key, h_value in hist_dict.items():
+      h = d.Filter(l_value).Histo1D(h_value, h_key)
+      hists.append(h)
+
+  #Baseline jet selection to shorten time
+  d = d.Filter('njet_jerc >= 3 && nbjet_jerc >= 2')
+
+  cut_dict = cut_flow(b_tagging(year))
+  for c_key, c_value in cut_dict.items():
+
+    lepton_dict = lepton_sel(year)
     for l_key, l_value in lepton_dict.items():
-      hist_dict = histos(l_key, 0, syst_ext)
+      hist_dict = histos(l_key, c_key, syst_int)
 
       for h_key, h_value in hist_dict.items():
-        h = d.Filter(l_value).Histo1D(h_value, h_key)
+        h = d.Filter(l_value + c_value, c_key).Histo1D(h_value, h_key, 'EventWeight')
         hists.append(h)
 
-    #Baseline jet selection to shorten time
-    d = d.Filter('njet_jerc >= 3 && nbjet_jerc >= 2')
+  for hist in hists:
+    hist.Write()
 
-    cut_dict = cut_flow(b_tagging(options.year))
-    for c_key, c_value in cut_dict.items():
+  out.Close()
+  print "Finished " + os.path.join(outDir, 'hist_' + folder_to_process.replace(syst_ext,'')) + postfix + syst_ext + '.root'
 
-      lepton_dict = lepton_sel(options.year)
-      for l_key, l_value in lepton_dict.items():
-        hist_dict = histos(l_key, c_key, syst_ext)
-
-        for h_key, h_value in hist_dict.items():
-          h = d.Filter(l_value + c_value, c_key).Histo1D(h_value, h_key)
-          hists.append(h)
-
-    for hist in hists:
-      hist.Write()
-
-    out.Close()
+if __name__ == '__main__':
+  pool = multiprocessing.Pool(56)
+  pool.map(run_ana, list_args)
+  pool.close()
+  pool.join()
+  #print "test"
 
 print(process.memory_info()[0]/(1024.0*1024)) #rss only
 print(process.cpu_times())
